e23d06a87a171f2b382b23110f999a4219fe865f
Nikich37 <nikita.maculevich37@gmail.com>
2021-07-19 23:50:19 +0300

    feature
    Added Mouse Event logick, but it doesn't work.

--- Program.js
+++ Program.js
@@ -12,6 +12,10 @@ var Shape = /** @class */ (function () {
     }
     return Shape;
 }());
+var x = 0;
+var y = 0;
+var oldX = 0;
+var oldY = 0;
 var DragAndDropApp = /** @class */ (function () {
     function DragAndDropApp() {
         var canvas = document.getElementById('canvas');
@@ -21,9 +25,11 @@ var DragAndDropApp = /** @class */ (function () {
             new Shape([new Point(100, 250), new Point(300, 250), new Point(300, 400), new Point(100, 400)]),
             new Shape([new Point(100, 450), new Point(300, 520), new Point(300, 550), new Point(200, 535), new Point(100, 570)])
         ];
+        this.oldPoints = this.shapes[0].points;
         this.canvas = canvas;
         this.context = context;
         this.redraw(context);
+        this.createUserEvents();
     }
     DragAndDropApp.prototype.redraw = function (context) {
         this.shapes.forEach(function (value) {
@@ -43,6 +49,83 @@ var DragAndDropApp = /** @class */ (function () {
                 context.stroke();
             }
             context.closePath();
+        });
+    };
+    DragAndDropApp.prototype.IsShape = function (x, y) {
+        var result = false;
+        var index = 0;
+        this.shapes.forEach(function (value) {
+            var shape = value;
+            var j = shape.points.length - 1;
+            for (var i = 0; i < shape.points.length; i++) {
+                if ((shape.points[i].y < y && shape.points[j].y >= y || shape.points[j].y < y && shape.points[i].y >= y) &&
+                    (shape.points[i].x + (y - shape.points[i].y) / (shape.points[j].y - shape.points[i].y) * (shape.points[j].x - shape.points[i].x) < x)) {
+                    result = !result;
+                }
+                j = i;
+            }
+            if (result == true) {
+                this.indexDragShape = index;
+            }
+            index++;
+        });
+        return result;
+    };
+    DragAndDropApp.prototype.WhichShape = function (x, y) {
+        var result = false;
+        var index = 0;
+        this.shapes.forEach(function (value) {
+            var shape = value;
+            var j = shape.points.length - 1;
+            for (var i = 0; i < shape.points.length; i++) {
+                if ((shape.points[i].y < y && shape.points[j].y >= y || shape.points[j].y < y && shape.points[i].y >= y) &&
+                    (shape.points[i].x + (y - shape.points[i].y) / (shape.points[j].y - shape.points[i].y) * (shape.points[j].x - shape.points[i].x) < x)) {
+                    result = !result;
+                }
+                j = i;
+            }
+            if (result == true) {
+                this.indexDragShape = index;
+            }
+            index++;
+        });
+        return index;
+    };
+    DragAndDropApp.prototype.createUserEvents = function () {
+        var _this = this;
+        var canvas = this.canvas;
+        canvas.addEventListener("mousedown", function (e) {
+            x = e.offsetX;
+            y = e.offsetY;
+            if (_this.IsShape(x, y)) {
+                _this.drag = true;
+                _this.oldPoints = _this.shapes[0].points;
+            }
+        });
+        canvas.addEventListener("mousemove", function (e) {
+            if (_this.drag) {
+                _this.context.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+                for (var i = 0; i < _this.shapes[0].points.length; i++) {
+                    _this.shapes[0].points[i].x = _this.oldPoints[i].x + x - oldX;
+                    _this.shapes[0].points[i].y = _this.oldPoints[i].y + y - oldY;
+                }
+                _this.redraw(_this.context);
+                x = e.offsetX;
+                y = e.offsetY;
+            }
+        });
+        canvas.addEventListener("mouseup", function (e) {
+            if (_this.drag) {
+                _this.context.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+                for (var i = 0; i < _this.shapes[0].points.length; i++) {
+                    _this.shapes[0].points[i].x = _this.oldPoints[i].x + x - oldX;
+                    _this.shapes[0].points[i].y = _this.oldPoints[i].y + y - oldY;
+                }
+                _this.redraw(_this.context);
+                x = 0;
+                y = 0;
+                _this.drag = false;
+            }
         });
     };
     return DragAndDropApp;
--- Program.ts
+++ Program.ts
@@ -16,12 +16,24 @@ class Shape{
         this.points = points;
         this.IsFill = true;
     }
+
+    
 }
 
+let x = 0;
+let y = 0;
+let oldX = 0;
+let oldY = 0;
 class DragAndDropApp{
     private canvas: HTMLCanvasElement;
     private context: CanvasRenderingContext2D;
+    private drag: boolean;
     private shapes: Shape[];
+    private oldPoints: Point[];
+
+    private indexDragShape: number;
+    private clickX: number;
+    private clickY: number;
 
     constructor(){
         let canvas = document.getElementById('canvas') as
@@ -33,11 +45,13 @@ class DragAndDropApp{
             new Shape([new Point(100, 250), new Point(300,250), new Point(300, 400), new Point (100, 400)]),
             new Shape([new Point(100, 450), new Point(300,520), new Point(300, 550), new Point (200, 535), new Point (100, 570)])
         ]
+        this.oldPoints = this.shapes[0].points;
 
         this.canvas = canvas;
         this.context = context;
 
         this.redraw(context);
+        this.createUserEvents();
     }
 
     private redraw(context: CanvasRenderingContext2D){
@@ -60,6 +74,90 @@ class DragAndDropApp{
             context.closePath();
         });
     }
+
+    public IsShape(x: number, y: number): boolean{
+        let result: boolean = false;
+        let index: number = 0;
+        this.shapes.forEach(function (value){
+            let shape: Shape = value;
+            let j: number = shape.points.length - 1;
+            for (let i: number = 0; i < shape.points.length; i++) {
+                if ( (shape.points[i].y < y && shape.points[j].y >= y || shape.points[j].y < y && shape.points[i].y >= y) &&
+                (shape.points[i].x + (y - shape.points[i].y) / (shape.points[j].y - shape.points[i].y) * (shape.points[j].x - shape.points[i].x) < x) ){
+                result = !result;
+                }
+                j = i;
+            }
+            if (result == true){
+                this.indexDragShape = index; 
+            }
+            index++;
+        });
+        return result;
+    }
+
+    public WhichShape(x: number, y: number): number{
+        let result: boolean = false;
+        let index: number = 0;
+        this.shapes.forEach(function (value){
+            let shape: Shape = value;
+            let j: number = shape.points.length - 1;
+            for (let i: number = 0; i < shape.points.length; i++) {
+                if ( (shape.points[i].y < y && shape.points[j].y >= y || shape.points[j].y < y && shape.points[i].y >= y) &&
+                (shape.points[i].x + (y - shape.points[i].y) / (shape.points[j].y - shape.points[i].y) * (shape.points[j].x - shape.points[i].x) < x) ){
+                result = !result;
+                }
+                j = i;
+            }
+            if (result == true){
+                this.indexDragShape = index; 
+            }
+            index++;
+        });
+        return index;
+    }
+
+    private createUserEvents() {
+        let canvas = this.canvas;
+    
+        canvas.addEventListener("mousedown", e => {
+            x = e.offsetX;
+            y = e.offsetY;
+            
+            if (this.IsShape(x, y)){
+            this.drag = true;
+            this.oldPoints = this.shapes[0].points;
+            }
+        });
+        canvas.addEventListener("mousemove", e =>{
+            if (this.drag){
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                for (let i = 0; i < this.shapes[0].points.length; i++){
+                this.shapes[0].points[i].x = this.oldPoints[i].x + x - oldX;
+                this.shapes[0].points[i].y = this.oldPoints[i].y + y - oldY;
+                }
+                this.redraw(this.context);
+                x = e.offsetX;
+                y = e.offsetY;
+            }
+        });
+        canvas.addEventListener("mouseup", e =>{
+            if (this.drag){
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                for (let i = 0; i < this.shapes[0].points.length; i++){
+                    this.shapes[0].points[i].x = this.oldPoints[i].x + x - oldX;
+                    this.shapes[0].points[i].y = this.oldPoints[i].y + y - oldY;
+                }
+                this.redraw(this.context);
+                x = 0;
+                y = 0;
+                this.drag = false;
+            }
+        });
+    
+    }
 }
+
+    
 
 new DragAndDropApp();
