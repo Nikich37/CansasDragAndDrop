a0dcf2759d4ac6c428ae2200ea96872af2359be1
Nikich37 <nikita.maculevich37@gmail.com>
2021-07-23 00:37:34 +0300

    delete
    Deleted old unused files.

--- dist/out-tsc/Shape.js
+++
@@ -1,8 +0,0 @@
-var Shape = /** @class */ (function () {
-    function Shape(points) {
-        this.points = points;
-        this.IsFill = false;
-    }
-    return Shape;
-}());
-export { Shape };
